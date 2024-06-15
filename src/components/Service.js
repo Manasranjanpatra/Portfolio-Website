5b0ac58c8cbb084231a0c8aa256831b2adb6cc4d
MANAS RANJAN PATRA <manasrpatra18@gmail.com>
2024-06-15 17:32:05 +0530

    LAST

--- src/components/Service.js
+++ src/components/Service.js
@@ -12,9 +12,7 @@ const Service = () => {
           <i className="fa-solid fa-code"></i>
           <h2>Web Designer</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, veniam.
-            Suscipit similique est laboriosam unde sit, quisquam molestiae?
-            Similique consectetur blanditiis accusantium deleniti tenetur et
+          Passionate Web Designer with a keen eye for visual design and user experience. Skilled in creating engaging, responsive websites  Dedicated to delivering innovative and user-centric digital solutions through collaborative efforts with clients and development teams.
           </p>
           <a href="/">Learn more</a>
         </div>
@@ -22,9 +20,7 @@ const Service = () => {
           <i className="fa-solid fa-crop"></i>
           <h2>UI/UX designer</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, veniam.
-            Suscipit similique est laboriosam unde sit, quisquam molestiae?
-            Similique consectetur blanditiis accusantium deleniti tenetur et
+          Enthusiastic UI/UX Designer passionate about creating intuitive and engaging user experiences.  Dedicated to producing innovative digital solutions through effective collaboration with clients and cross-functional teams.
           </p>
           <a href="/">Learn more</a>
         </div>
@@ -40,7 +36,7 @@ const Service = () => {
         </div>
         <div>
           <i className="fa-solid fa-pen-to-square"></i>
-          <h2>Frontend Developer</h2>
+          <h2>Backend Developer</h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, veniam.
             Suscipit similique est laboriosam unde sit, quisquam molestiae?
@@ -50,7 +46,7 @@ const Service = () => {
         </div>
         <div>
           <i className="fa-solid fa-pen-to-square"></i>
-          <h2>Frontend Developer</h2>
+          <h2>Video Editor</h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, veniam.
             Suscipit similique est laboriosam unde sit, quisquam molestiae?
@@ -60,7 +56,7 @@ const Service = () => {
         </div>
         <div>
           <i className="fa-solid fa-pen-to-square"></i>
-          <h2>Frontend Developer</h2>
+          <h2> Photo Grapher</h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, veniam.
             Suscipit similique est laboriosam unde sit, quisquam molestiae?
